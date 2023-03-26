4d1cc1f53a015b8ec442226ee6a6638e2bef609e
Ibukunoluwa Omotola <ibkomotola4real@gmail.com>
2023-03-26 07:27:24 -0700

    printf format

--- 1-printf.c
+++ 1-printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - formatted output conversion and print data
+ * @format: format
  *
- * Return: number of chars printed.
+ * Return: number of chars printed
  */
 int _printf(const char *format, ...)
 {
